3d2597ddc31f85e64eaf88ad8dd1569abe5efb90
Alvaro <ahernandezvalvarde@iessonferrer.net>
2021-09-29 20:20:52 +0200

    Initial commit

---
+++ AHernandez/Divisores.php
@@ -0,0 +1,26 @@
+<html lang="es">
+<head>
+    <title>Get divisors</title>
+</head>
+<body>
+<form method="post" action="Divisores.php">
+    <label>
+        Number:
+        <input type="text" name="num"/>
+    </label>
+    <input type="submit"/>
+</form>
+<div>
+    <?php
+    if (isset($_POST["num"])) {
+        $num = intval($_POST["num"]);
+        for ($i = 1; $i < $num; $i++) {
+            if ($num % $i == 0) {
+                echo $i;
+            }
+        }
+    }
+    ?>
+</div>
+</body>
+</html>

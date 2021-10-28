ec9cb67d3f19d1c87b1504f962b155e28a039797
Alvaro <ahernandezvalvarde@iessonferrer.net>
2021-10-28 17:03:59 +0200

    28-10-21

--- AHernandez/Congreso de los Diputados/arrays.php
+++ AHernandez/Congreso de los Diputados/arrays.php
@@ -1,11 +1,20 @@
 <?php
 
-    $provincias=['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres',
-        'Cádiz','Cantabria','Castellón','Ciudad Real','Ceuta','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara',
-        'Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Melilla','Murcia','Navarra',
-        'Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
-        'Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
-    $partidos=["PP","PSOE","Podemos","Vox","Pacma"];
+$provincias = array('Alava', 'Albacete', 'Alicante', 'Almería', 'Asturias', 'Avila', 'Badajoz', 'Barcelona', 'Burgos', 'Cáceres',
+    'Cádiz', 'Cantabria', 'Castellón', 'Ciudad Real', 'Ceuta', 'Córdoba', 'La Coruña', 'Cuenca', 'Gerona', 'Granada', 'Guadalajara',
+    'Guipúzcoa', 'Huelva', 'Huesca', 'Islas Baleares', 'Jaén', 'León', 'Lérida', 'Lugo', 'Madrid', 'Málaga', 'Melilla', 'Murcia', 'Navarra',
+    'Orense', 'Palencia', 'Las Palmas', 'Pontevedra', 'La Rioja', 'Salamanca', 'Segovia', 'Sevilla', 'Soria', 'Tarragona',
+    'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya', 'Zamora', 'Zaragoza');
+$partidos = array("PP", "PSOE", "Podemos", "Vox", "Pacma");
 
-    var_dump($provincias);
+
+$circunscripcion = array($provincias=>array());
+for ($i = 0; $i < count($provincias); $i++) {
+
+
+    for ($j = 0; $j < count($partidos); $j++) {
+        $circunscripcion[$provincias[$i]][] = $partidos[$j];
+    }
+}
+var_dump($circunscripcion);
 ?>

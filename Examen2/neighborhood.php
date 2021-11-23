52a49c82c00948c84e0e227a3a5f057a895dff27
Alvaro <ahernandezvalvarde@iessonferrer.net>
2021-11-23 18:52:20 +0100

    23-11-21

---
+++ Examen2/neighborhood.php
@@ -0,0 +1,36 @@
+<?php
+
+class neighborhood
+{
+    private $id, $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+//*******************************************
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+}

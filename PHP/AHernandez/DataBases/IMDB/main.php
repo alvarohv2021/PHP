9af1123095e78459613f358244aebfe0caecdaf3
Alvaro <alvarohv2013@gmail.com>
2021-12-09 14:15:07 +0100

    9-12-21

--- PHP/AHernandez/DataBases/IMDB/main.php
+++ PHP/AHernandez/DataBases/IMDB/main.php
@@ -2,60 +2,52 @@
 include 'funciones.php';
 global $arrayOBJ_Peliculas;
 echo '<style>
-ul{
-list-style-type: none;
+
+table,td{
+border-collapse: collapse;
 }
+
+img{
+width: 50%;
+height: 50%;
+}
+.id{
+width: 5%;
+}
+.name{
+width: 69%;
+}
+.clasicicacion{
+width: 5%;
+}
+
 </style>';
 /*echo '<pre>';
 var_dump($arrayOBJ_Peliculas);
 echo '</pre>';*/
 
-echo '<h1>Peliculas</h1>';
-for ($i = 0; $i < count($arrayOBJ_Peliculas); $i++) {
-    $pelicula = $arrayOBJ_Peliculas[$i];
-    echo '<title>Ranking</title>
-<div>
-    <ul class="id">
-        <li><img src=>' . $pelicula->getFoto().'</li>
-    </ul>
-</div>';
-}
-for ($i = 0; $i < count($arrayOBJ_Peliculas); $i++) {
-    $pelicula = $arrayOBJ_Peliculas[$i];
-    echo '<title>Ranking</title>
-<div>
-    <ul class="id">
-        <li>' . $pelicula->getId().'</li>
-    </ul>
-</div>';
-}
-for ($i = 0; $i < count($arrayOBJ_Peliculas); $i++) {
-    $pelicula = $arrayOBJ_Peliculas[$i];
 echo '
-<div>
-    <ul class="name">
-        <li><a href="google.com"> ' . $pelicula->getName() . '</a></li>
-    </ul>
-</div>';
-}
+<h1>Peliculas</h1>
+<div class="todo">
+    <table>';
 for ($i = 0; $i < count($arrayOBJ_Peliculas); $i++) {
     $pelicula = $arrayOBJ_Peliculas[$i];
-echo '
-<div>
-    <ul class="estreno">
-        <li>('.$pelicula->getEstreno().')</li>
-    </ul>
-</div>';
+if ($i%2==0){
+    $color="grey";
+}else{
+    $color="white";
 }
-for ($i = 0; $i < count($arrayOBJ_Peliculas); $i++) {
-    $pelicula = $arrayOBJ_Peliculas[$i];
-    echo '
-<div>
-    <ul class="calificacion">
-        <li>'.$pelicula->getCalificacion().'</li>
-    </ul>
-</div>';
+    echo '<title>Ranking</title>
+<tr style="background-color: '.$color.'">
+    <td class="foto"><img src=Fotos/' . $pelicula->getFoto() . '></td>
+    <td class="id"">' . $pelicula->getId() . '</td>
+    <td class="name"><a href="google.com"> ' . $pelicula->getName() . '</a> (' . $pelicula->getEstreno() . ')</td>
+    <td class="clasificacion">' . $pelicula->getCalificacion() . '</td>
+</tr>';
+
 }
+echo '</table>
+</div>';
 
 
 ?>
